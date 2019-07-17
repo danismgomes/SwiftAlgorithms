9badc8aaaa7ffd3c2a3c0dce8bedafd81f2d961f
danismgomes <danismgomes@gmail.com>
2019-07-17 09:52:01 -0700

    first commit

---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Danielle Gomes on 2019-07-17.
+//  Copyright © 2019 Danielle Gomes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
